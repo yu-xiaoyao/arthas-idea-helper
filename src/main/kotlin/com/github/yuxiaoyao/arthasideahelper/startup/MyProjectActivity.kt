6b1b31176c253fbd1952f66b8db224ccfbdec619
GitHub Action <action@github.com>
2025-08-14 03:52:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuxiaoyao/arthasideahelper/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yuxiaoyao/arthasideahelper/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yuxiaoyao.arthasideahelper.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
